31229a456ceb3a7a707dacecea10ab973c429a1b
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 10:00:28 +0800

    合并默认参数

--- index.js
+++ index.js
@@ -4,3 +4,12 @@
  * @createTime 2018年5月16日 上午9:47:07
  */
 
+module.exports = (config, outPath) => {
+    config = {
+        title: '',
+        author: 'pashangshangpo',
+        publisher: 'Pashangshangpo & Co.',
+        cover: '',
+        ...config
+    }
+}
