72e275421cfcd23c00277924d8c9d2cc1c4d0d8f
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 13:46:47 +0800

    修改cover信息

--- template/cover.js
+++ template/cover.js
@@ -6,19 +6,21 @@
 
 /**
  * .def: coverPath => string
+ *   name: String 名称
  *   coverPath: String cover路径
  */
-module.exports = coverPath => {
+module.exports = (name, coverPath) => {
     return `
 <!DOCTYPE html SYSTEM "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>cover</title>
+    <title>封面</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
 <body>
     <div>
-        <img src="${coverPath}" alt="Comic Book Images"/>
+        <h2>${name}</h2>
+        ${coverPath ? `<img src="${coverPath}" alt="Comic Book Images"/>`: ''}
     </div>
 </body>
 </html>
