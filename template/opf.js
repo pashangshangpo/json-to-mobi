85182d207ce1b2c0baf696f117598911a0c19ae6
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 19:16:18 +0800

    修改模板内容

--- template/opf.js
+++ template/opf.js
@@ -20,27 +20,21 @@ module.exports = (config, chapters) => {
 <?xml version="1.0" encoding="UTF-8"?>
 <package xmlns:xx="http://saxon.sf.net/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/metadata/dublin_core" unique-identifier="BookId" version="2.0">
     <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
-        <meta name="fixed-layout" content="true"/>
-        <meta name="orientation-lock" content="landscape"/>
-        <meta name="original-resolution" content="1024x600"/>
-        <meta name="book-type" content="children"/>
-        <meta name="RegionMagnification" content="true"/>
         <dc-metadata xmlns:dc="http://purl.org/metadata/dublin_core" xmlns:oebpackage="http://openbook.org/namespaces/oeb-package/1.0/"> 
             <dc:title>${config.name}</dc:title>
             <dc:language>${config.lang}</dc:language>
             <dc:creator>${config.author}</dc:creator>
             <dc:publisher>${config.publisher}</dc:publisher>
             <dc:date>${config.date}</dc:date>
-            <meta name="cover" content="my_cover_image" />
         </dc-metadata>
         <x-metadata>
             <EmbeddedCover>${config.cover}</EmbeddedCover>
         </x-metadata>
     </metadata>
     <manifest>
-        <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
         <item id="stylesheet" href="style/global.css" media-type="text/css"/>
-        <item id="cover" href="toc.html" media-type="application/xhtml+xml"/>
+        <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+        <item id="toc" href="toc.html" media-type="application/xhtml+xml"/>
         ${chapters.map((item, index) => {
             return `
                 <item id="page-${index}" href="pages/page-${index}.html" media-type="application/xhtml+xml"/>
@@ -48,13 +42,17 @@ module.exports = (config, chapters) => {
         }).join('').trim()}
     </manifest>
     <spine toc="ncx">
-        <itemref idref="cover"/>
+        <itemref idref="toc"/>
         ${chapters.map((item, index) => {
             return `
              <itemref idref="page-${index}"/>
             `
         }).join('').trim()}
     </spine>
+    <guide>
+        <reference type="toc" title="目录" href="toc.html"/>
+        <reference type="text" title="首页" href="pages/page-0.html"/>
+    </guide>
 </package>
     `
 }
