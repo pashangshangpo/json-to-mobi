afa9877cdfd71873b29b793de8f3d553be51af20
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 19:08:09 +0800

    去除不需要的数据

--- template/ncx.js
+++ template/ncx.js
@@ -17,18 +17,14 @@ module.exports = (name, navList) => {
 <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
 <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1" xml:lang="en">
     <head>
-        <meta name="dtb:uid" content="content"/>
-        <meta name="dtb:depth" content="1"/>
-        <meta name="dtb:totalPageCount" content="0"/>
-        <meta name="dtb:maxPageNumber" content="0"/>
     </head>
     <docTitle>
         <text>${name}</text>
     </docTitle>
     <navMap>
-        <navPoint id="navpoint-1" playOrder="1">
+        <navPoint id="toc" playOrder="1">
             <navLabel>
-                <text xmlns:ns="http://www.daisy.org/z3986/2005/ncx/">${name}</text>
+                <text>${name}</text>
             </navLabel>
             <content src="toc.html"/>
         </navPoint>
@@ -36,7 +32,7 @@ module.exports = (name, navList) => {
             return `
                 <navPoint id="navpoint-${index + 2}" playOrder="${index + 2}">
                     <navLabel>
-                        <text xmlns:ns="http://www.daisy.org/z3986/2005/ncx/">${item.title}</text>
+                        <text>${item.title}</text>
                     </navLabel>
                     <content src="pages/page-${index}.html"/>
                 </navPoint>
