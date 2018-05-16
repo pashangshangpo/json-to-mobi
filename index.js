e46c45d70007803523d9a7580990783927358858
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 12:50:35 +0800

    添加忽略

--- index.js
+++ index.js
@@ -4,8 +4,12 @@
  * @createTime 2018年5月16日 上午9:47:07
  */
 
+const fs = require('fs')
+
 const ncxTemplate = require('./template/ncx')
 const opfTemplate = require('./template/opf')
+const coverTemplate = require('./template/cover')
+const pageTemplate = require('./template/page')
 
 module.exports = (config, outPath) => {
     config = {
@@ -15,6 +19,11 @@ module.exports = (config, outPath) => {
         publisher: 'Pashangshangpo & Co.',
         date: new Date().getFullYear(),
         cover: 'images/cover.png',
+        chapters: [],
         ...config
     }
+
+    // fs.writeFile()
+    toc.ncx
+    ncxTemplate(config.name, config.chapters)
 }
