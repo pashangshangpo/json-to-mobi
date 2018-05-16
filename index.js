5db4f660f2f56b1c4583b649540bf476f9181b28
pashangshangpo <pashangshangpo@gmail.com>
2018-05-16 10:07:40 +0800

    修改目录名

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = (config, outPath) => {
         title: '',
         author: 'pashangshangpo',
         publisher: 'Pashangshangpo & Co.',
-        cover: './image/cover.png',
+        cover: './images/cover.png',
         ...config
     }
 }
