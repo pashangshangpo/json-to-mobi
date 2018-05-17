955350c979d0ec28ed36ba8396a4f4510223209f
pashangshangpo <pashangshangpo@gmail.com>
2018-05-17 15:24:59 +0800

    添加下载图片方法

---
+++ common/downImage.js
@@ -0,0 +1,40 @@
+/**
+ * @file 下载图片
+ * @author pashangshangpo
+ * @createTime 2018年5月17日 下午3:23:10
+ */
+
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const { basename, join } = require('path')
+
+module.exports = (url, outDir) => {
+    let type = http
+    if (url.indexOf('https') === 0) {
+        type = https
+    }
+
+    return new Promise(resolve => {
+        type.get(url, res => {
+            let data = ''
+
+            res.setEncoding('binary')
+
+            res.on('data', chunk => {
+                data += chunk
+            })
+
+            res.on('end', () => {
+                fs.writeFile(join(outDir, basename(url)), data, 'binary', err => {
+                    if (err) {
+                        throw err
+                        return
+                    }
+                    
+                    resolve()
+                })
+            })
+        })
+    })
+}
